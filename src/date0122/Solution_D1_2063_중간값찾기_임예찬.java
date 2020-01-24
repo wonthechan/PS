447f21364941ffe3e1c104d877e5771c21c16014
wonthechan <packerstorm@gmail.com>
2020-01-24 21:32:36 +0900

    init commit

---
+++ src/date0122/Solution_D1_2063_중간값찾기_임예찬.java
@@ -0,0 +1,50 @@
+﻿package date0122;
+
+import java.util.Scanner;
+
+public class Solution_D1_2063_중간값찾기_임예찬 {
+
+	public static int[] scores;
+	public static int N;
+	
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int temp;
+		int medianKey;
+		
+		// 점수 개수 입력
+		N = sc.nextInt();
+			
+		// 점수를 담을 배열 생성
+		scores = new int[N];
+		
+		// 점수를 입력 받아 배열에 저장
+		for (int i = 0; i < N; i++) {
+			scores[i] = sc.nextInt();
+		}
+		
+		// 오름차순으로 정렬 (선택 정렬) 진행
+		for (int i = 0; i < N - 1; i++) {
+			int minKey = i;
+			for (int j = i + 1; j < N; j++) {
+				if (scores[j] < scores[minKey]) {
+					minKey = j;
+				}
+			}
+			if (i == minKey) continue;
+			// swap
+			temp = scores[minKey];
+			scores[minKey] = scores[i];
+			scores[i] = temp;
+		}
+		
+		// 중간값 인덱스 설정
+		medianKey = (N / 2) + 1;
+		
+		// 중간값 출력
+		System.out.println(scores[medianKey-1]);
+		
+		sc.close();
+	}
+}
---
+++ src/date0122/Solution_D2_1204_최빈수구하기_임예찬.java
@@ -0,0 +1,54 @@
+package date0122;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution_D2_1204_최빈수구하기_임예찬 {
+
+	public static int[] scoreFreqArr;
+	
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		int score;
+		int maxKey;
+		
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			// 점수 빈도를 기록할 배열 생성 (0으로 초기화)
+			// 매번 배열을 새로 생성하는 경우 공간복잡도 증가 가능성 있음
+			// Arrays.fill(scoreFreqArr, 0);
+			scoreFreqArr = new int[101];
+			// 테스트 케이스 수 입력 받은 것 날려버리기
+			sc.nextInt();
+			
+			// 1000명의 점수를 입력받는다.
+			for (int i = 0; i < 1000; i++) {
+				score = sc.nextInt();
+				// 해당 점수에 해당되는 배열의 원소 값을 증가
+				scoreFreqArr[score]++;
+			}
+			
+			// 빈도 배열을 순회하면서 최댓값 원소의 인덱스를 구한다.
+			maxKey = 1;
+			for (int i = 2; i < 101; i++) {
+				if (scoreFreqArr[i] >= scoreFreqArr[maxKey]) {
+					// 점수의 오름차순으로 탐색하기 때문에 빈도수가 같은 경우 높은 점수의 빈도수로 교체된다.
+					maxKey = i;
+				}
+//				if (scoreFreqArr[i] > scoreFreqArr[maxKey]) {
+//					maxKey = i;
+//				} else if (scoreFreqArr[i] == scoreFreqArr[maxKey]) {
+//					// 최빈수가 여러 개 일 때에는 가장 큰 점수를 출력
+//					maxKey = (i > maxKey)? i : maxKey;
+//				}	
+			}
+			
+			// 빈도 배열의 인덱스가 곧 점수 이므로 최빈수는 maxKey가 된다.
+			System.out.println("#" + test_case + " " + maxKey);
+		}
+		sc.close();
+	}
+}
