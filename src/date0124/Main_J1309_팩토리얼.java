b2181a322db3e5ac3f1126638ce21e7ea8e5809d
wonthechan <packerstorm@gmail.com>
2020-01-25 14:29:29 +0900

    1차 제출

---
+++ src/date0124/Main_J1309_팩토리얼.java
@@ -0,0 +1,38 @@
+package date0124;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_J1309_팩토리얼 {
+
+	// 팩토리얼의 숫자 n (1<=n<=15)
+	static int n;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			// 시작범위와 끝 범위를 입력 받는다.
+			n = Integer.parseInt(br.readLine());
+			if ((n >= 1 && n <= 15)) {
+				// 올바른 범위에 있는 경우
+				break;
+			} else {
+				// 주어진 범위를 벗어날 경우
+				System.out.println("INPUT ERROR!");
+			}
+		}
+		
+		System.out.println(printFactorial(n));
+	}
+	private static long printFactorial(int n) {
+		if (n == 1) {
+			System.out.printf("%d! = 1\n", n);
+			return 1;
+		} else {
+			System.out.printf("%d! = %d * %d!\n", n, n, n - 1);
+		}
+		return n * printFactorial(n-1);
+	}
+}
