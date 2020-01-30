2f144a884867d13b5ab9a4d73c015408ec81848c
wonthechan <packerstorm@gmail.com>
2020-01-30 09:36:05 +0900

    주석 제거

--- src/hw/date0123/Solution_D4_1210_Ladder1_임예찬.java
+++ src/hw/date0123/Solution_D4_1210_Ladder1_임예찬.java
@@ -43,7 +43,6 @@ public class Solution_D4_1210_Ladder1_임예찬 {
 					break;
 				}
 			}
-			
 			System.out.println("#" + tcNum + " " + answer);
 		}
 	}
@@ -54,7 +53,6 @@ public class Solution_D4_1210_Ladder1_임예찬 {
 		}
 	}
 	private static boolean dfs(int i, int j) {
-//		System.out.println("START: " + "i = " + i + ", j = " + j);
 		if (visit[i][j] == true) {
 			// 이미 방문한적이 있는 경우
 			return false;
@@ -74,43 +72,33 @@ public class Solution_D4_1210_Ladder1_임예찬 {
 				// 첫 행에 있는 경우
 				if (ladder[i][j+1] == 1 && visit[i][j+1] == false) {
 					// 오른쪽으로 갈 수 있는 경우
-//					System.out.println(1);
 					return dfs(i, j+1);
 				} else {
-//					System.out.println(2);
 					return dfs(i+1, j);
 				}
 			} else if (j == N-1) {
 				// 마지막 행에 있는 경우
 				if (ladder[i][j-1] == 1 && visit[i][j-1] == false) {
 					// 왼쪽으로 갈 수 있는 경우
-//					System.out.println(3);
 					return dfs(i, j-1);
 				} else {
-//					System.out.println(4);
 					return dfs(i+1, j);
 				}
 			} else {
 				// 중간 행에 있는 경우
 				if (ladder[i][j+1] == 1 && visit[i][j+1] == false) {
 					// 왼쪽으로 갈 수 있는 경우
-//					System.out.println(5);
 					return dfs(i, j+1);
 				}
 				if (ladder[i][j-1] == 1 && visit[i][j-1] == false) {
 					return dfs(i, j-1);
 				}
-//					System.out.println(6);
 				return dfs(i+1, j);
 			}
 		} else if (ladder[i][j] == 2) {
 			// 정답을 찾은 경우
 			return true;
 		}
-		
-//		System.out.println("i = " + i + ", j = " + j);
 		return false;
 	}
-	
-	
 }
