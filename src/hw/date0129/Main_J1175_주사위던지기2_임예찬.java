9a56943a2f22887311193a8b0163c04a599fc643
wonthechan <packerstorm@gmail.com>
2020-01-30 11:16:01 +0900

    주석추가

--- src/hw/date0129/Main_J1175_주사위던지기2_임예찬.java
+++ src/hw/date0129/Main_J1175_주사위던지기2_임예찬.java
@@ -24,9 +24,29 @@ public class Main_J1175_주사위던지기2_임예찬 {
 		numbers = new int[N];	// 뽑는 개수 만큼 크기 생성 (유동적)
 		
 		permutation(0, 0);
+//		permutation1(0, 0, "");
+	}
+
+	private static void permutation1(int index, int sum, String str) {
+		/* 뽑은 주사위 눈을 매개변수로 가져간다. */
+		if (index == N) {
+			// 지금 까지 뽑은 숫자가 담겨있는 배열의 합 구하기
+			if(sum == M) {
+				System.out.println(str);
+			}
+			return;
+		}
+		for (int i = 1; i <= 6; i++) {
+			// sum 매개변수에 방금 뽑은 수를 누적.
+			permutation1(index+1, sum + i, str + i + " ");
+		}
 	}
 
 	private static void permutation(int index, int sum) {
+		// 현재까지 던진 주사위눈의 합이 목표합을 이미 초과해버린 경우 나머지 경우를 더이상 보지 않는다. => return
+		// 가지치기
+		if (sum > M) return;
+		
 		if (index == N) {
 			// 지금 까지 뽑은 숫자가 담겨있는 배열의 합 구하기
 			if(sum == M) {
@@ -36,6 +56,7 @@ public class Main_J1175_주사위던지기2_임예찬 {
 			return;
 		}
 		
+		// 6개의 선택지를 가진다.
 		for (int i = 1; i <= 6; i++) {
 			numbers[index] = i;
 			// sum 매개변수에 방금 뽑은 수를 누적.
