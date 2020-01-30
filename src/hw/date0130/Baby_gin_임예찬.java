cf2c2556b5ccd7cb3e21eeaea5987e8c6a64a4d2
wonthechan <packerstorm@gmail.com>
2020-01-30 19:02:20 +0900

    2차 완성

--- src/hw/date0130/Baby_gin_임예찬.java
+++ src/hw/date0130/Baby_gin_임예찬.java
@@ -3,7 +3,6 @@ package hw.date0130;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 /*
  *  0 ~ 9 사이의 숫자 카드에서 임의의 카드 6장을 뽑았을 때, 
@@ -12,80 +11,80 @@ import java.util.Arrays;
  *  그리고, 6장의 카드가 run과 triplet로만 구성된 경우를  baby-gin으로 부른다
  *  6자리의 숫자를 입력 받아 baby-gin 여부를 판단하는 프로그램을 작성하라.
  */
-public class BabyGin {
+public class Baby_gin_임예찬 {
 
-	static final int N = 6;
-	static int[] numbers;
-	static int[] result;
-	static boolean[] selected;
+	static final int N = 6;		// 6장의 숫자 카드를 뽑을 수 있음
+	static int[] numbers;		// 뽑은 6개의 숫자가 저장된 배열
+	static int[] result;		// 앞서 뽑은 6개의 숫자로 만들 수 있는 순열이 저장된 배열
+	static boolean[] selected;	// 순열을 만들기 위해 사용되는 방문 확인 배열
 	static boolean isBabyGin;
 	
 	public static void main(String[] args) throws IOException {
-		// 기본적으로 입력 받은 6자리 수의 모든 순열을 구해본 후 Baby-gin 에 해당하는 경우를 구해본다. 그다음 절반을 나누어 체크
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
 		
 		numbers = new int[N];
 		result = new int[N];
 		selected = new boolean[N];
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) {	// 입력 받은 6개의 숫자를 배열로 저장한다.
 			numbers[i] = Integer.parseInt(str.charAt(i) + "");
 		}
 		
-		isBabyGin = false;
-		perm(0);
+		isBabyGin = false;				// 초기 상태는 false로 지정한다.
+		generatePerm(0);				// 순열을 만들어내는 메소드를 실행
 		
 		if(isBabyGin) {
-			System.out.println("Baby-gin !!!");
+			System.out.println("Baby-gin 입니다!!");
 		} else {
-			System.out.println("Not a Baby-gin...");
+			System.out.println("Baby-gin 이 아닙니다..");
 		}
 	}
 
-	private static void perm(int index) {
+	/* 입력받은 6자리의 수로 만들 수 있는 모든 순열을 구한다 */
+	private static void generatePerm(int index) {
 		if (isBabyGin) return;	// BabyGin이 성립되는 경우를 찾았다면 더 이상 진행하지 않음
 		
 		if (index == N) {	// 기저조건
-			// 절반으로 쪼개서 Baby-gin인 경우가 있는지 체크
-			if (checkBabyGin(result.clone()))	isBabyGin = true;
+			// 해당 순열이  BabyGin인지 확인하는 메소드 실행 (배열을 문자열로 변환하여 매개변수로 전달)
+			String toStr = "";
+			for (int i = 0; i < N; i++) toStr += result[i];
+			if (checkBabyGin(toStr))	isBabyGin = true;
 			return;
 		}
 		for (int i = 0; i < N; i++) {
 			if (selected[i]) continue;
 			result[index] = numbers[i];
 			selected[i] = true;
-			perm(index + 1);
+			generatePerm(index + 1);
 			selected[i] = false;
 		}
 	}
 
-	private static boolean checkBabyGin(int[] arr) {
-		
-		String first = "";
-		String second = "";
-		
-		for (int i = 0; i < 3; i++)	first += arr[i];
-		for (int i = 3; i < N; i++)	second += arr[i];
-
+	/* 6개의 숫자를 3개씩 쪼개어서 두 개 모두 Run 또는 Triplet에 해당되는지 확인하여
+	 * Baby-gin 여부를 리턴한다.
+	 */
+	private static boolean checkBabyGin(String str) {
+		String first = str.substring(0, 3);
+		String second = str.substring(3);
+		// Baby-gin이 되는 경우를 조사한다.
 		if ((checkRun(first) && checkTriplet(second)) || (checkRun(second) && checkTriplet(first)) ||
 				(checkRun(first) && checkRun(second)) || (checkTriplet(first) && checkTriplet(second))) {
 			return true;
 		}
-		
 		return false;
 	}
 
+	/* 처음 숫자와 두번째 숫자와 같고 두번째 숫자가 마지막 숫자와 같다면 Triplet으로 판단 */
 	private static boolean checkTriplet(String str) {
 		if (str.charAt(0) == str.charAt(1) && str.charAt(1) == str.charAt(2)) return true;
 		return false;
 	}
 
+	/* 연속으로 감소하거나 증가하는 경우 Run으로 판단 */
 	private static boolean checkRun(String str) {
 		if (str.charAt(2) - str.charAt(1) == 1 && str.charAt(1) - str.charAt(0) == 1) return true;
 		if (str.charAt(2) - str.charAt(1) == -1 && str.charAt(1) - str.charAt(0) == -1) return true;
 		return false;
 	}
 	
-	
-
 }
