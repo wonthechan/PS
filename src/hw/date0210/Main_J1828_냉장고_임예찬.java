8654fa7bd3b73db7021d20e3ed17b4f2476ad29e
wonthechan <packerstorm@gmail.com>
2020-02-10 18:21:35 +0900

    푸는중

---
+++ src/hw/date0210/Main_J1828_냉장고_임예찬.java
@@ -0,0 +1,47 @@
+package hw.date0210;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_J1828_냉장고_임예찬 {
+	
+	static int[] tempMin;
+	static int[] tempMax;
+	static int N;
+	static int[] arr;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		N = Integer.parseInt(br.readLine());
+		
+		tempMin = new int[N];
+		tempMax = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			tempMin[i] = Integer.parseInt(st.nextToken());
+			tempMax[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int minTemp = tempMin[0];
+		for (int t : tempMin) minTemp = Math.min(minTemp, t);
+		
+		int maxTemp = tempMax[0];
+		for (int t : tempMin) maxTemp = Math.min(maxTemp, t);
+		
+		int size = maxTemp - minTemp;
+		
+		arr = new int[size];
+		
+		for (int i = 0; i < N; i++) {
+			int tempSize = tempMax[i] - tempMin[i];
+			for (int j = 0; j <= tempSize; j++) {
+				
+			}
+		}
+		
+	}
+
+}
