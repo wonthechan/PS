3615e10a2298d4916b7383c06f3b438c34c67122
Yechan Lim <34850678+wonthechan@users.noreply.github.com>
2020-02-10 19:58:08 +0900

    Update Main_J1828_냉장고_임예찬.java

--- src/hw/date0210/Main_J1828_냉장고_임예찬.java
+++ src/hw/date0210/Main_J1828_냉장고_임예찬.java
@@ -1,7 +1,9 @@
 package hw.date0210;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_J1828_냉장고_임예찬 {
@@ -11,6 +13,7 @@ public class Main_J1828_냉장고_임예찬 {
 	static int N;
 	static int[] arr;
 	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("input_j1828.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		
@@ -32,23 +35,56 @@ public class Main_J1828_냉장고_임예찬 {
 		for (int t : tempMin) maxTemp = Math.min(maxTemp, t);
 		
 		// 선택정렬 (최저온드 정렬)
-		int minIdx = 0;
-		int minValue = tempMin[0];
-		for (int i = 0; i < N; i++) {
-			
+		for (int i = 0; i < N-1; i++) {
+			int minIdx = i;
+			int minValue = tempMin[i];
+			for (int j = i+1; j < N; j++) {
+				if (tempMin[j] < minValue) {
+					minIdx = j;
+					minValue = tempMin[j];
+				}
+			}
+			if (minIdx == i) continue; // 자신보다 더 작은 원소를 발견하지 못한 경우
+			// swap
+			int minX = tempMin[minIdx];
+			int minY = tempMax[minIdx];
+			tempMin[minIdx] = tempMin[i];
+			tempMax[minIdx] = tempMax[i];
+			tempMin[i] = minX;
+			tempMax[i] = minY;
 		}
+
 		
+		System.out.println(Arrays.toString(tempMin));
+		System.out.println(Arrays.toString(tempMax));
 		
-		int size = maxTemp - minTemp;
+		int cnt = 1;
 		
-		arr = new int[size];
-		
-		for (int i = 0; i < N; i++) {
-			int tempSize = tempMax[i] - tempMin[i];
-			for (int j = 0; j <= tempSize; j++) {
-				
+		int curMax = tempMax[0];
+		for (int i = 1; i < N; i++) {
+			
+			int nextMin = tempMin[i];
+			int nextMax = tempMax[i];
+			
+			if (nextMin > curMax) { // 범위가 겹치지 않는 경우 냉장고가 하나 더 필요함
+				System.out.printf("(%d ~ %d)\n", nextMin, nextMax);
+				cnt++;
+				curMax = nextMax;
 			}
 		}
+		
+		System.out.println(cnt);
+		
+//		int size = maxTemp - minTemp;
+//		
+//		arr = new int[size];
+//		
+//		for (int i = 0; i < N; i++) {
+//			int tempSize = tempMax[i] - tempMin[i];
+//			for (int j = 0; j <= tempSize; j++) {
+//				
+//			}
+//		}
 		
 	}
 
