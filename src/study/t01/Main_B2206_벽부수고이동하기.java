ea793af037b0aa1d20bb8c056ce6fd0f5edbe583
wonthechan <packerstorm@gmail.com>
2020-02-18 18:35:33 +0900

    푸는중2

--- src/study/t01/Main_B2206_벽부수고이동하기.java
+++ src/study/t01/Main_B2206_벽부수고이동하기.java
@@ -3,12 +3,16 @@ package study.t01;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_B2206_벽부수고이동하기 {
 
 	static int N, M;
-	static int[][] map;
+	static char[][] map;
+	static boolean[][] visit;
+	static int dist = 1;
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("input_b2206.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,21 +22,77 @@ public class Main_B2206_벽부수고이동하기 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
+		// map과 visit 배열 초기화
+		map = new char[N][M];
+		visit = new boolean[N][M];
+		
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
+			String line = br.readLine();
 			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j] = line.charAt(j);
 			}
 		}
 		
-		
+//		System.out.println(bfs());	// (0,0) 부터 시작 해서 (N-1, M-1)에 도달
+		if (bfs()) System.out.println(dist);
+		else System.out.println(-1);
+	}
+					//	상	하	좌	우
+	static int[] dy4 = {-1, 1, 0, 0};
+	static int[] dx4 = {0, 0, -1, 1};
+	
+	private static boolean bfs() {
+		Queue<Pos> queue = new LinkedList<>();
+		visit[0][0] = true;
+		queue.offer(new Pos(0,0, false));
+		while(!queue.isEmpty()) {
+			int size = queue.size();
+			while(size-- > 0) {
+				Pos out = queue.poll();
+				if (out.y == N-1 && out.x == M-1) {
+					return true;
+				}
+				for (int dir = 0; dir < 4; dir++) {
+					int ny = out.y + dy4[dir];
+					int nx = out.x + dx4[dir];
+					if (ny >= 0 && nx >= 0 && ny < N && nx < M 
+							&& visit[ny][nx] == false ) {
+						if (map[ny][nx] == '0') {
+							System.out.println("* " + ny + ", " + nx + ", " + out.chance);
+							visit[ny][nx] = true;
+							queue.offer(new Pos(ny, nx, out.chance));
+						} else {
+							if (!out.chance) {
+								for (int dir2 = 0; dir2 < 4; dir2++) {
+									int nny = ny + dy4[dir2];
+									int nnx = nx + dx4[dir2];
+									if (nny >= 0 && nnx >= 0 && nny < N && nnx < M 
+											&& visit[nny][nnx] == false && map[nny][nnx] == '0') {
+//										System.out.println(ny + ", " + nx);
+										System.out.println("** " + ny + ", " + nx + ", " + out.chance);
+//										out.chance = true;
+										map[ny][nx] = '0';
+										visit[ny][nx] = true;
+										queue.offer(new Pos(ny, nx, true));
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			++dist;
+		}
+		return false;
 	}
 	
 	static class Pos {
-		int x, y;
-		public Pos(int y, int x) {
+		int x, y, dir;
+		boolean chance;
+		public Pos(int y, int x, boolean chance) {
 			this.y = y;
 			this.x = x;
+			this.chance = chance;
 		}
 	}
 }
