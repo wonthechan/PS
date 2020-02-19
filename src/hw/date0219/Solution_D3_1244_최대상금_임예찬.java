69c1a68983c3c801a5e21843885ad97ea75c0910
wonthechan <packerstorm@gmail.com>
2020-02-19 22:59:53 +0900

    2차제출

--- src/hw/date0219/Solution_D3_1244_최대상금_임예찬.java
+++ src/hw/date0219/Solution_D3_1244_최대상금_임예찬.java
@@ -1,6 +1,7 @@
 package hw.date0219;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
@@ -8,46 +9,59 @@ import java.util.StringTokenizer;
 public class Solution_D3_1244_최대상금_임예찬 {
 
 	static int[] numbers;
-	static int N;
+	static int N, R;
+	static int maxScore;
+	static boolean[][] visit;
 	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("input_s1244.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		
 		int T = Integer.parseInt(br.readLine());
 		
 		for (int tc = 1; tc <= T; tc++) {
-			int answer = 0;
+			maxScore = 0;
 			
 			st = new StringTokenizer(br.readLine());
 			String str = st.nextToken();
-			int r = Integer.parseInt(st.nextToken());
+			R = Integer.parseInt(st.nextToken());
 			N = str.length();
 			numbers = new int[N];
+			visit = new boolean[R][1000000];	// R번째 교환 시 방문한 적이 있는지
 			for (int i = 0; i < N; i++) {
 				numbers[i] = str.charAt(i) - '0';
 			}
 			
-			
-			
-			
-			
-			
-			
-			System.out.println("#" + tc + " " + answer);
+			dfs(0);
+			System.out.println("#" + tc + " " + maxScore);
 		}
 	}
 	
-	private static void permutation(int idx) {
-		if (idx == R) {
-			++totalCount;
-			System.out.println(Arrays.toString(numbers));
+	private static void dfs(int cnt) {
+		int curBonus = computeBonus();		// 현재 보너스 금액을 계산
+		if (cnt == R) {						// 교환이 모두 끝난 경우
+			maxScore = Math.max(maxScore, curBonus);
 			return;
 		}
+		if (visit[cnt][curBonus]) return;	// 이미 구한적이 있다면 pass
+		visit[cnt][curBonus] = true;
 		for (int i = 0; i < N; i++) {
-			swap(idx, i);
-			permutation(idx + 1);
-			swap(idx, i);
+			for (int j = i + 1; j < N; j++) {
+				swap(i, j);
+				dfs(cnt + 1);
+				swap(i, j);
+			}
 		}
+	}
+	
+	private static int computeBonus() {
+		int bonus = 0;
+		int mul = 1;
+		for (int i = N - 1; i >= 0; i--) {
+			bonus += numbers[i] * mul;
+			mul *= 10;
+		}
+		return bonus;
 	}
 	
 	private static void swap(int a, int b) {	// 인자는 인덱스를 받는다.
