915dfad3c19fbd3b7666a3ee64ca709db333b3b8
wonthechan <packerstorm@gmail.com>
2020-03-08 16:37:10 +0900

    1차 제출

---
+++ src/study/date0308/Main_B14891_톱니바퀴.java
@@ -0,0 +1,78 @@
+package study.date0308;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_B14891_톱니바퀴 {
+
+	static char[][] arr = new char[4][8];
+	static int answer = 0;
+	static int[] isRotate = new int[4];
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("input_b14891.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		for (int i = 0; i < 4; i++) {
+			arr[i] = br.readLine().toCharArray();
+		}
+		int K = Integer.parseInt(br.readLine());
+		for (int i = 0; i < K; i++) {
+			st = new StringTokenizer(br.readLine());
+			rotate(Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken()));
+		}
+		
+		// 점수 계산
+		for (int i = 0; i < 4; i++) {
+			if (arr[i][0] == '1') answer += Math.pow(2, i);
+		}
+		System.out.println(answer);
+	}
+	
+	private static void rotate(int k, int way) {
+		Arrays.fill(isRotate, 0);
+		int curWay;
+		// 초기 상태를 보고 회전할 수 있는지 미리 판단
+		curWay = way;
+		for (int i = k-1; i >= 0; i--) {
+			if (arr[i][2] != arr[i+1][6]) {
+				curWay *= -1;
+				isRotate[i] = curWay;	// 방향도 함께 저장
+			} else {
+				break;
+			}
+		}
+		curWay = way;
+		for (int i = k+1; i < 4; i++) {
+			if (arr[i][6] != arr[i-1][2]) {
+				curWay *= -1;
+				isRotate[i] = curWay;
+			} else {
+				break;
+			}
+		}
+		// 일단 k번째 톱니바퀴를 움직이고
+		rotateOne(k, way);
+		// 앞서 판단한 결과로 나머지 바퀴들을 움직인다.
+		for (int i = 0; i < 4; i++) {
+			if (isRotate[i] != 0) rotateOne(i, isRotate[i]);
+		}
+	}
+	
+	private static void rotateOne(int k, int way) {
+		// k번재 톱니바퀴를 움직인다. (way : 1 = 시계방향)
+		char temp;
+		if (way == 1) {
+			temp = arr[k][7];
+			for (int i = 7; i > 0; i--) arr[k][i] = arr[k][i-1];
+			arr[k][0] = temp;
+		} else {
+			temp = arr[k][0];
+			for (int i = 0; i < 7; i++) arr[k][i] = arr[k][i+1];
+			arr[k][7] = temp;
+		}
+	}
+}
