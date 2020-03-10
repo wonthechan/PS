f94ab9579114d4adc7f51fabb62facae9c38ce28
wonthechan <packerstorm@gmail.com>
2020-03-10 15:53:37 +0900

    경로 변경

--- src/study/date0310/Main_B15684_사다리조작.java
+++ src/study/date0310/Main_B15684_사다리조작.java
@@ -1,4 +1,4 @@
-package study.date0309;
+package study.date0310;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -11,7 +11,6 @@ public class Main_B15684_사다리조작 {
 		System.setIn(new FileInputStream("input_b15684.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
-		
 		
 	}
 
