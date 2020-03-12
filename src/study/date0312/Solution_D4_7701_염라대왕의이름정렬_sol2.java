63eb0669b3ed1ce5a0e06e1c00168f2714d60935
wonthechan <packerstorm@gmail.com>
2020-03-12 16:43:29 +0900

    update

--- src/study/date0312/Solution_D4_7701_염라대왕의이름정렬_sol2.java
+++ src/study/date0312/Solution_D4_7701_염라대왕의이름정렬_sol2.java
@@ -7,36 +7,43 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.TreeSet;
 
-/* 트리셋 자료구조를 이용한 풀이 */
-public class Solution_D4_7701_염라대왕의이름정렬_sol {
+/* 분할 정복 아이디어 추가 */
+public class Solution_D4_7701_염라대왕의이름정렬_sol2 {
 
 	static int N;	// 이름 개수 (1 <= N <= 20000)
-	static TreeSet<String> set;
+	static TreeSet<String>[] sets = new TreeSet[50];
+	static String out;
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("input_s7701.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
 			sb.append("#").append(tc).append("\n");
-			set = new TreeSet<>(new Comparator<String>() {
-				@Override
-				public int compare(String o1, String o2) {
-					int diff = o1.length() - o2.length();
-					if (diff == 0) return o1.compareTo(o2);
-					return diff;	// 문자열의 길이가 짧은 순으로 정렬
-				}
-			});
+			for (int i = 0; i < 50; i++) {
+				sets[i] = new TreeSet<>(new Comparator<String>() {
+					@Override
+					public int compare(String o1, String o2) {
+						int diff = o1.length() - o2.length();
+						if (diff == 0) return o1.compareTo(o2);
+						return diff;	// 문자열의 길이가 짧은 순으로 정렬
+					}
+				});
+			}
 			
 			N = Integer.parseInt(br.readLine());
 			
+			String str;
 			for (int i = 0; i < N; i++) {
-				set.add(br.readLine());
+				str = br.readLine();
+				sets[str.length()-1].add(str);
 			}
 			
-			Iterator<String> itr = set.iterator();
-			while (itr.hasNext()) {
-				sb.append(itr.next()).append("\n");
+			for (int i = 0; i < 50; i++) {
+				for (String name : sets[i]) {
+					sb.append(name).append("\n");
+				}
 			}
 		}
 		System.out.print(sb.toString());
