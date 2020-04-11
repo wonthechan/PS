63e4409eb2286306df646e83933c80c6166ad40e
wonthechan <packerstorm@gmail.com>
2020-04-11 17:00:49 +0900

    클래스명 변경

--- src/study/date0411/Main_B1305_광고.java
+++ src/study/date0411/Main_B1305_광고.java
@@ -5,7 +5,7 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main_B1306_광고 {
+public class Main_B1305_광고 {
 
 	static int L;
 	public static void main(String[] args) throws Exception {
