603c40f535a0d5b3a144131039a10d4f3f33160e
wonthechan <packerstorm@gmail.com>
2020-04-16 23:00:27 +0900

    코드 수정

--- src/study/date0416/Main_B2579_계단오르기.java
+++ src/study/date0416/Main_B2579_계단오르기.java
@@ -22,12 +22,9 @@ public class Main_B2579_계단오르기 {
 		if (n > 1) {
 			dp[2] = Math.max(stairs[2], stairs[1] + stairs[2]);
 		}
-		if (n > 2) {
-			dp[3] = Math.max(stairs[2] + stairs[3], stairs[1] + stairs[3]);
-		}
 		
 		// 오른 계단의 수는 중요하지 않음
-		for (int i = 4; i <= n; i++) {
+		for (int i = 3; i <= n; i++) {
 			dp[i] = Math.max(dp[i-3] + stairs[i-1] + stairs[i], dp[i-2] + stairs[i]);
 		}
 		
