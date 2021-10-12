14978ce0ca46798a19e55c3c9cd549a1600e96a7
Yechan <packerstorm@gmail.com>
2021-10-13 07:00:54 +0900

    21년 문제풀이 패키지 생성

---
+++ src/study/y21/date1009/Main_B1260_DFS와BFS_211009.java
@@ -0,0 +1,12 @@
+package study.y21.date1009;
+
+public class Main_B1260_DFS와BFS_211009 {
+	
+	static int N;
+	static int M;
+	static int V;
+	
+	public static void main(String[] args) {
+		System.out.println("Hi!");
+	}
+}
